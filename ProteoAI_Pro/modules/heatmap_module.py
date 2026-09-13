b56ac4b16642941d49383797afdb37c695588b20
educationassist25 <education.assist25@gmail.com>
2026-09-12 21:42:11 -0500

    Shorten colorbar label from 'Z-score (row-scaled normalized intensity)' to 'Z-Score'

--- ProteoAI_Pro/modules/heatmap_module.py
+++ ProteoAI_Pro/modules/heatmap_module.py
@@ -560,7 +560,7 @@ def clustered_heatmap(data_log2: pd.DataFrame, significant_features, meta: pd.Da
 
     ax_cbar = fig.add_axes([cbar_left, cbar_bottom, cbar_w, cbar_h_in / H])
     cbar = fig.colorbar(im, cax=ax_cbar, orientation="horizontal")
-    cbar.set_label("Z-score (row-scaled normalized intensity)", fontsize=0.7 * font_size, fontfamily=font_family)
+    cbar.set_label("Z-Score", fontsize=0.7 * font_size, fontfamily=font_family)
     cbar.ax.tick_params(labelsize=0.6 * font_size)
     for lbl in cbar.ax.get_xticklabels():
         lbl.set_fontfamily(font_family)
